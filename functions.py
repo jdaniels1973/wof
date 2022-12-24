5b6780e476ea30877069e8033dc04f17a5c0f836
Jonathan Daniels <118066190+jdaniels1973@users.noreply.github.com>
2022-12-24 14:28:49 +0000

    new commit

---
+++ functions.py
@@ -0,0 +1,2 @@
+def sortedList(e):
+  return e['id']
---
+++ main.py
@@ -0,0 +1,434 @@
+import functions
+
+Skywing = 0
+Mudwing = 0
+Seawing = 0
+Rainwing = 0
+Nightwing = 0
+Icewing = 0
+Sandwing = 0
+highestNumber = 0
+
+
+# Function for printing current choices
+def PrintVal():
+  print("VALUES: " + str(Skywing) + ", " + str(Mudwing) + ", " + str(Seawing) +
+        ", " + str(Rainwing) + ", " + str(Nightwing) + ", " + str(Icewing) +
+        ", " + str(Sandwing))
+
+
+print("Which Wings of Fire tribe do you belong in?")
+
+question1 = input( "Where would you like to live?Please answer a,b,c,d,e,f,g.... a. Mountain, b. swamp, c. ocean, d. rainforest, e. volcano, f. tundra, g. desert")
+
+if question1 == "a":
+  Skywing = Skywing + 1
+
+elif question1 == "b":
+  Mudwing = Mudwing + 1
+
+elif question1 == "c":
+  Seawing = Seawing + 1
+
+elif question1 == "d":
+  Rainwing = Rainwing + 1
+
+elif question1 == "e":
+  Nightwing = Nightwing + 1
+
+elif question1 == "f":
+  Icewing = Icewing + 1
+
+elif question1 == "g":
+  Sandwing = Sandwing + 1
+
+else:
+  print("Enter a to g only")
+
+#PrintVal()
+
+question2 = input(
+  " What is your favourite color pallete? please answer a,b,c,d,e,f,g.... a. Red, yellow, orange, b. shades of brown, c. greens and blues, d. rainbow, e. black, purple, dark blue, f. White, silver, light blue , g. golds and yellows  "
+)
+
+if question2 == "a":
+  Skywing = Skywing + 1
+
+elif question2 == "b":
+  Mudwing = Mudwing + 1
+
+elif question2 == "c":
+  Seawing = Seawing + 1
+
+elif question2 == "d":
+  Rainwing = Rainwing + 1
+
+elif question2 == "e":
+  Nightwing = Nightwing + 1
+
+elif question2 == "f":
+  Icewing = Icewing + 1
+
+elif question2 == "g":
+  Sandwing = Sandwing + 1
+
+else:
+  print("Enter a to g only")
+
+#PrintVal()
+
+question3 = input(
+  " Which power would you want? please answer a,b,c,d,e,f,g.... a. Fire breath, b. Extreme strength, c. Breathing underwater, d. venom fangs, e. Mindreading, f. Ice breath , g. Scorpian tail  "
+)
+
+if question3 == "a":
+  Skywing = Skywing + 1
+
+elif question3 == "b":
+  Mudwing = Mudwing + 1
+
+elif question3 == "c":
+  Seawing = Seawing + 1
+
+elif question3 == "d":
+  Rainwing = Rainwing + 1
+
+elif question3 == "e":
+  Nightwing = Nightwing + 1
+
+elif question3 == "f":
+  Icewing = Icewing + 1
+
+elif question3 == "g":
+  Sandwing = Sandwing + 1
+
+else:
+  print("Enter a to g only")
+
+#PrintVal()
+
+question4 = input(
+  "  choose a dragon name. please answer a,b,c,d,e,f,g.... a. Peril, b. Clay, c. Tsunami, d. Glory, e. Starflight, f. Winter , g. Thorn  "
+)
+
+if question4 == "a":
+  Skywing = Skywing + 1
+
+elif question4 == "b":
+  Mudwing = Mudwing + 1
+
+elif question4 == "c":
+  Seawing = Seawing + 1
+
+elif question4 == "d":
+  Rainwing = Rainwing + 1
+
+elif question4 == "e":
+  Nightwing = Nightwing + 1
+
+elif question4 == "f":
+  Icewing = Icewing + 1
+
+elif question4 == "g":
+  Sandwing = Sandwing + 1
+
+else:
+  print("Enter a to g only")
+
+#PrintVal()
+
+question5 = input(
+  " What is your favourite month? please answer a,b,c,d,e,f,g.... a.June,july , b. March, c. September, d. april,may, e. Oct,nov, f. Dec,Jan , g. August  "
+)
+
+if question5 == "a":
+  Skywing = Skywing + 1
+
+elif question5 == "b":
+  Mudwing = Mudwing + 1
+
+elif question5 == "c":
+  Seawing = Seawing + 1
+
+elif question5 == "d":
+  Rainwing = Rainwing + 1
+
+elif question5 == "e":
+  Nightwing = Nightwing + 1
+
+elif question5 == "f":
+  Icewing = Icewing + 1
+
+elif question5 == "g":
+  Sandwing = Sandwing + 1
+
+else:
+  print("Enter a to g only")
+
+#PrintVal()
+
+question6 = input(
+  " What is your favourite weather? please answer a,b,c,d,e,f,g.... a.  cloudy, b. slush , c. rain, d. rainbows, e. stormy, f. snowing , g. sunny "
+)
+
+if question6 == "a":
+  Skywing = Skywing + 1
+
+elif question6 == "b":
+  Mudwing = Mudwing + 1
+
+elif question6 == "c":
+  Seawing = Seawing + 1
+
+elif question6 == "d":
+  Rainwing = Rainwing + 1
+
+elif question6 == "e":
+  Nightwing = Nightwing + 1
+
+elif question6 == "f":
+  Icewing = Icewing + 1
+
+elif question6 == "g":
+  Sandwing = Sandwing + 1
+
+else:
+  print("Enter a to g only")
+
+#PrintVal()
+
+question7 = input(
+  " What is your element? please answer a,b,c,d,e,f,g.... a. Air, b. Earth, c. Water, d. Plant, e. Space, f. Ice , g. Fire  "
+)
+
+if question7 == "a":
+  Skywing = Skywing + 1
+
+elif question7 == "b":
+  Mudwing = Mudwing + 1
+
+elif question7 == "c":
+  Seawing = Seawing + 1
+
+elif question7 == "d":
+  Rainwing = Rainwing + 1
+
+elif question7 == "e":
+  Nightwing = Nightwing + 1
+
+elif question7 == "f":
+  Icewing = Icewing + 1
+
+elif question7 == "g":
+  Sandwing = Sandwing + 1
+
+else:
+  print("Enter a to g only")
+
+#PrintVal()
+
+question8 = input(
+  " What is your favourite animal? please answer a,b,c,d,e,f,g.... a. Birds, b. Lizards, c. Dolphins, d. sloth, e. Bat, f. Wolves , g. Scorpian  "
+)
+
+if question8 == "a":
+  Skywing = Skywing + 1
+
+elif question8 == "b":
+  Mudwing = Mudwing + 1
+
+elif question8 == "c":
+  Seawing = Seawing + 1
+
+elif question8 == "d":
+  Rainwing = Rainwing + 1
+
+elif question8 == "e":
+  Nightwing = Nightwing + 1
+
+elif question8 == "f":
+  Icewing = Icewing + 1
+
+elif question8 == "g":
+  Sandwing = Sandwing + 1
+
+else:
+  print("Enter a to g only")
+
+#PrintVal()
+
+question9 = input(
+  " What is your favourite food? please answer a,b,c,d,e,f,g.... a.  Poultry, b. Beef, c. seafood, d. fruit, e. Human flesh, f. ice cold foods, like ice cream, g. spicy food  "
+)
+
+if question9 == "a":
+  Skywing = Skywing + 1
+
+elif question9 == "b":
+  Mudwing = Mudwing + 1
+
+elif question9 == "c":
+  Seawing = Seawing + 1
+
+elif question9 == "d":
+  Rainwing = Rainwing + 1
+
+elif question9 == "e":
+  Nightwing = Nightwing + 1
+
+elif question9 == "f":
+  Icewing = Icewing + 1
+
+elif question9 == "g":
+  Sandwing = Sandwing + 1
+
+else:
+  print("Enter a to g only")
+
+#PrintVal()
+
+question10 = input(
+  " What do you do for fun ? please answer a,b,c,d,e,f,g.... a. Make others fight in an arena, b. Play in the mud, c. Go swimming, d. sleep, e. capture an experiment on others, f. play in the snow , g. Shop in the market  "
+)
+
+if question10 == "a":
+  Skywing = Skywing + 1
+
+elif question10 == "b":
+  Mudwing = Mudwing + 1
+
+elif question10 == "c":
+  Seawing = Seawing + 1
+
+elif question10 == "d":
+  Rainwing = Rainwing + 1
+
+elif question10 == "e":
+  Nightwing = Nightwing + 1
+
+elif question10 == "f":
+  Icewing = Icewing + 1
+
+elif question10 == "g":
+  Sandwing = Sandwing + 1
+
+else:
+  print("Enter a to g only")
+
+#PrintVal()
+
+question11 = input(
+  " What is your personality ? please answer a,b,c,d,e,f,g.... a. Grumpy, b. Hungry, c. Adventurous, d. Peacful, e. Mysterious, f. demanding , g. Loyal   "
+)
+
+if question11 == "a":
+  Skywing = Skywing + 1
+
+elif question11 == "b":
+  Mudwing = Mudwing + 1
+
+elif question11 == "c":
+  Seawing = Seawing + 1
+
+elif question11 == "d":
+  Rainwing = Rainwing + 1
+
+elif question11 == "e":
+  Nightwing = Nightwing + 1
+
+elif question11 == "f":
+  Icewing = Icewing + 1
+
+elif question11 == "g":
+  Sandwing = Sandwing + 1
+
+else:
+  print("Enter a to g only")
+
+PrintVal()
+
+# Create a DICTIONARY
+NamesList = [
+  {
+    "id": "a",
+    "count": Skywing
+  },
+  {
+    "id": "b",
+    "count": Mudwing
+  },
+  {
+    "id": "c",
+    "count": Seawing
+  },
+  {
+    "id": "d",
+    "count": Rainwing
+  },
+  {
+    "id": "e",
+    "count": Nightwing
+  },
+  {
+    "id": "f",
+    "count": Icewing
+  },
+  {
+    "id": "g",
+    "count": Sandwing
+  },
+]
+
+listedNames = list(NamesList)
+listedNames.sort(key=lambda x: x["count"], reverse=False)
+
+#print("LIST ITEM: " + str(listedNames[4].get("id")))
+#print("LIST VALUE: " + str(listedNames[4].get("count")))
+#print("LIST SIZE = " + str(listedNames.__len__()))
+
+dSize2=listedNames.__len__()
+iCount2=0
+highestID="none"
+highestVal=0
+# iSortedList=items
+# for key in iSortedList:
+while iCount2 < dSize2:
+  #print("ID IS " + str(listedNames[iCount2].get("id")))
+  #print ("VALUE IS " + str(listedNames[iCount2].get("count")))
+
+  # Check if current val is higher than highest val and record it if so
+  if listedNames[iCount2].get("count") > highestVal:
+    highestVal = listedNames[iCount2].get("count")
+    highestID = listedNames[iCount2].get("id")
+    
+  iCount2+=1
+
+#print ("HIGHEST VAL is " + str(highestVal))
+#print ("HIGHEST ID = " + str(highestID))
+
+  
+#NamesList.sort(key=sortedList)
+# s_NamesList=list(NamesList.items())
+
+# CALCULATE highest value using "max" function on NamesList
+##highestNumber = max(Skywing, Mudwing, Seawing, Rainwing, Nightwing, Icewing, Sandwing)
+##print ("HIGHEST is ..." + str(highestNumber))
+
+##print ("TOP CHOICE IS: " + sorted(NamesList.sort(count.items(7)))
+
+if str(highestID)=="a":
+  print("You are a Skywing!")
+elif str(highestID)=="b":
+  print("You are a Mudwing!")
+elif str(highestID) == "c":
+  print("You are a Seawing!")
+elif str(highestID) == "d":
+  print("You are a Rainwing!")
+elif str(highestID) == "e":
+  print("You are a Nightwing!")
+elif str(highestID) == "f":
+  print("You are a Icewing!")
+elif str(highestID) == "g":
+  print("You are a Sandwing!")
+else:
+  print ("Not sure")
